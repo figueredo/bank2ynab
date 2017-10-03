38a5ee2d040f587daa49d5e69b1a326d861be8f6
Thiago Cardoso <tfcardoso@gmail.com>
2017-10-02 22:12:47 -0300

    Add Itaucard's input mapper

---
+++ src/input/itaucard.js
@@ -0,0 +1,28 @@
+import { Statement, StatementType } from '../statement';
+
+function mapDate(dayStr, monthStr, year) {
+  return new Date(year, parseInt(monthStr, 10) - 1, parseInt(dayStr, 10));
+}
+
+function mapAmount(amountStr) {
+  return Math.abs(parseFloat(amountStr.replace(',', '.')));
+}
+
+function mapType(amountStr) {
+  return amountStr.startsWith('-') ? StatementType.INFLOW : StatementType.OUTFLOW;
+}
+
+export default function mapFactory(year) {
+  return function map(entry) {
+    const regex = /^(\d{2})\/(\d{2})\s+(.+)\s+(-?\d+,\d{2})$/i;
+    const match = entry.match(regex);
+    if (!match) {
+      return entry;
+    }
+    const date = mapDate(match[1], match[2], year);
+    const payee = match[3];
+    const amount = mapAmount(match[4]);
+    const type = mapType(match[4]);
+    return new Statement(date, payee, amount, type);
+  };
+}
